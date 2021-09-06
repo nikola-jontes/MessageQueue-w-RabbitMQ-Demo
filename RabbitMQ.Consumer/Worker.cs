ab8c08fc776574b9439ff3d4b7b399ab996dba5f
BlackAdder <55687222+AdditionSnake@users.noreply.github.com>
2021-09-06 11:56:39 +0200

    Consumer Test Code

--- RabbitMQ.Consumer/Worker.cs
+++ RabbitMQ.Consumer/Worker.cs
@@ -3,8 +3,11 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQ.Consumer
 {
@@ -22,9 +25,31 @@ namespace RabbitMQ.Consumer
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-                Console.WriteLine("GitHub?");
+                await Task.Delay(5*1000, stoppingToken);
+                ConsumeMessage();
             }
+        }
+
+        void ConsumeMessage()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("queue-person-service");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("queue-person-service", true, consumer);
         }
     }
 }
